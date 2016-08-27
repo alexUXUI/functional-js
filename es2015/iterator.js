36b9496e251f3ea4b495710802e340f26b5fcf7a
Alex Bennett <alexandergraybennett@gmail.com>
2016-08-27 10:51:19 -0600

    generator as iterator

--- es2015/iterator.js
+++ es2015/iterator.js
@@ -1,7 +1,9 @@
-var animals = {
-  1: 'cat',
-  2: 'dog',
-  3: 'turtle'
+'use strict'
+
+let animals = {
+  1 : 'cat',
+  2 : 'dog',
+  3 : 'turtle'
 }
 
 function *iterator(obj) {
@@ -9,13 +11,13 @@ function *iterator(obj) {
   let keys = Reflect.ownKeys(obj)
 
   for (let key of keys) {
-    yield console.log([key, obj[key]]);
+    yield console.log([key, obj[key]])
   }
 
 }
 
 let animal = iterator(animals);
 
-animal.next() // [ '1', 'cat' ]
-animal.next() // [ '2', 'dog' ]
-animal.next() // [ '3', 'turtle' ]
+animal.next() // => [ '1', 'cat' ]
+animal.next() // => [ '2', 'dog' ]
+animal.next() // => [ '3', 'turtle' ]
